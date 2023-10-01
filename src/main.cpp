3bf51cc9a57bf34fda374384908c9f5d973fa75d
Edouard Gomez <ed.gomez@free.fr>
2023-10-01 10:36:10 +0200

    build: plug in SDL3 for fun

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <cstdlib>
+#include <cstdio>
+
+#include <SDL3/SDL.h>
+
+namespace
+{
+void ReportSDLVersion()
+{
+    SDL_version sdl_version;
+    SDL_GetVersion(&sdl_version);
+    std::printf("julia: using SDL %d.%d\n", sdl_version.major, sdl_version.minor);
+}
+
+}
 
 extern "C" int main(int argc, char** argv)
 {
+    ReportSDLVersion();
+
     return EXIT_SUCCESS;
 }
