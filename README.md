fc3a7f2fb2f66f1c7143c56af358a42427602eaf
Edouard Gomez <ed.gomez@free.fr>
2023-10-01 10:16:30 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# Julia fractal
+
+Small code smaple to draw julia fractals in a SDL2 application
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <cstdlib>
+
+extern "C" int main(int argc, char** argv)
+{
+    return EXIT_SUCCESS;
+}
