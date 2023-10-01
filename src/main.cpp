3c8842030b4f1b3eb3ab46cd26941cdbd0ec1e8e
Edouard Gomez <ed.gomez@free.fr>
2023-10-01 14:31:13 +0200

    app: initialize the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,60 @@
 #include <cstdlib>
 #include <cstdio>
+#include <functional>
 
 #include <SDL3/SDL.h>
 
+#include "scope_exit.h"
+
 namespace
 {
-void ReportSDLVersion()
-{
-    SDL_version sdl_version;
-    SDL_GetVersion(&sdl_version);
-    std::printf("julia: using SDL %d.%d\n", sdl_version.major, sdl_version.minor);
-}
+
+constexpr int s_window_default_witdh = 640;
+constexpr int s_window_default_height = 480;
 
 }
 
 extern "C" int main(int argc, char** argv)
 {
-    ReportSDLVersion();
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SCOPE_EXIT([]() { SDL_Quit(); });
+
+    SDL_Window* window = nullptr;
+    window = SDL_CreateWindow("Julia fractal", s_window_default_witdh, s_window_default_height, 0);
+    if (!window)
+    {
+        std::printf("error: failed creating window\n");
+        return EXIT_FAILURE;
+    }
+    SCOPE_EXIT([window]() {SDL_DestroyWindow(window); });
+
+    SDL_Renderer* renderer = nullptr;
+    renderer = SDL_CreateRenderer(window, nullptr, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
+    if (!renderer)
+    {
+        std::printf("error: failed creating renderer\n");
+        return EXIT_FAILURE;
+    }
+    SCOPE_EXIT([renderer]() { SDL_DestroyRenderer(renderer); });
+
+    bool quit = false;
+    while (!quit)
+    {
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+        {
+            switch (e.type)
+            {
+                case SDL_EVENT_QUIT:
+                    quit = true;
+                    break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
 
     return EXIT_SUCCESS;
 }
---
+++ src/scope_exit.h
@@ -0,0 +1,54 @@
+#pragma once
+
+namespace
+{
+
+/** RAII scope guard */
+template<typename T>
+struct scope_exit
+{
+public:
+    /** ctor
+     * @param[in] f Functor to execute on scope exit
+     */
+    explicit scope_exit(T&& f) : m_functor(std::forward<T>(f)) {}
+    ~scope_exit() { if (!m_disabled) { m_functor(); } }
+    void drop() const { m_disabled = true; }
+
+    /* disable copy, makes no sense */
+    scope_exit(scope_exit const &) = delete;
+    scope_exit &operator=(scope_exit const &) = delete;
+
+private:
+    T m_functor; /**< functor executed on scope exit through the dtor */
+    mutable bool m_disabled = false; /**< the scope exit object may be disabled by the user */
+};
+
+/** scope_exit creator
+ * @param[in] f Functor to execute on scope exit
+ */
+template<typename T>
+scope_exit<T> make_scope_exit(T&& f) { return scope_exit<T>(std::forward<T>(f)); }
+
+#define DETAIL_SCOPE_EXIT_CONCATX(a, b) a ## b
+#define DETAIL_SCOPE_EXIT_CONCAT(a, b) DETAIL_SCOPE_EXIT_CONCATX(a, b)
+
+/** Macro helper to define a named scope_exit object. Use this if it is
+ * required to drop the guard eventually in a sort of init like codepath
+ * reaching its ending, and willing to disarm all guards defined along
+ * the codepath to handle its cleanup
+ * @param[in] varname Variable name to be defined
+ * @param[in] f Functor to be executed on scope exit
+ */
+#define SCOPE_EXIT_NAMED(varname, f) const auto& varname = make_scope_exit(f)
+
+/** Macro helper to define a scope_exit object. Use this if the scope_exit
+ * object is not to be disarmed and its name is of no value.
+ * Its variable name will automatically be set to scope_exit_ ## __LINE__
+ * to avoid any conflicts within a same compilation unit.
+ * @param[in] varname Variable name to be defined
+ * @param[in] f Functor to be executed on scope exit
+ */
+#define SCOPE_EXIT(f) SCOPE_EXIT_NAMED(DETAIL_SCOPE_EXIT_CONCAT(scope_exit_, __LINE__), f)
+
+}
