391be664e0f4facb1621cf76df75261a0ed70785
Edouard Gomez <ed.gomez@free.fr>
2023-10-15 21:55:33 +0200

    app: add command line parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Copyright(C) 2023 Edouard Gomez
 
+#include <climits>
 #include <complex>
 #include <cstdio>
 #include <cstdlib>
@@ -14,40 +15,183 @@
 namespace
 {
 
-constexpr int s_window_default_witdh = 640;
-constexpr int s_window_default_height = 480;
-
-template <typename T> using unique_ptr_custom_delete = std::unique_ptr<T, std::function<void(T*)>>;
-
 using complex = std::complex<float>;
 
-// You can try different constant complex here.
-// See https://paulbourke.net/fractals/juliaset/
-complex julia_c{0.355534f, -0.337292f};
+bool
+parse_number(unsigned int& val, char const* str)
+{
+    unsigned long v;
+    char* endptr = nullptr;
+    v = std::strtoul(str, &endptr, 0);
+    if ((errno == ERANGE && v == ULONG_MAX) || endptr == str)
+    {
+        val = 0;
+        return false;
+    }
 
-// Displacement value when key pressing r/R or i/I
-static constexpr complex julia_c_displacement{0.001f, 0.001f};
+    val = static_cast<unsigned int>(v);
+    return true;
+}
 
-// Number of iterations before stating the norm of the series reaches infinity
-static constexpr int s_iterations = 64;
+bool
+parse_number(float& val, char const* str)
+{
+    float v;
+    char* endptr = nullptr;
+    v = std::strtof(str, &endptr);
+    if ((errno == ERANGE && v == HUGE_VALF) || (v == 0.f && endptr == str))
+    {
+        val = 0.f;
+        return false;
+    }
 
-// This is infinity here ;-)
-static constexpr float s_arbitrary_big_norm = 1000.f;
+    val = v;
+    return true;
+}
+
+template <typename T> bool
+parse_arg(T& dst, int i, int argc, char** argv, bool& should_quit, int& retval)
+{
+    bool parsed_sucessfully;
+
+    if (i < (argc - 1))
+    {
+        T v;
+        parsed_sucessfully = parse_number(v, argv[i + 1]);
+        if (!parsed_sucessfully)
+        {
+            retval = EXIT_FAILURE;
+            should_quit = true;
+        }
+
+        dst = v;
+    }
+    else
+    {
+        retval = EXIT_FAILURE;
+        should_quit = true;
+        parsed_sucessfully = false;
+    }
+
+    return parsed_sucessfully;
+}
+
+struct app_config
+{
+    unsigned int width{s_default_window_witdh};
+    unsigned int height{s_default_window_height};
+
+    // You can try different constant complex here.
+    // See https://paulbourke.net/fractals/juliaset/
+    complex julia_c{s_default_julia_c};
+
+    complex julia_c_displacement{s_julia_c_displacement};
+
+    unsigned int iterations{s_default_iterations};
+
+    float infinity{s_default_infinity};
+
+    bool
+    parse_cmdline(int argc, char** argv, int& retval)
+    {
+        bool should_quit = false;
+
+        for (int i = 0; i < argc; ++i)
+        {
+            std::string_view argv_str(argv[i]);
+            if (argv_str == "--iterations")
+            {
+                parse_arg(iterations, i, argc, argv, should_quit, retval);
+            }
+            else if (argv_str == "--infinity")
+            {
+                parse_arg(infinity, i, argc, argv, should_quit, retval);
+            }
+            else if (argv_str == "--cst-r")
+            {
+                float real;
+                if (parse_arg(real, i, argc, argv, should_quit, retval))
+                {
+                    julia_c = {real, julia_c.imag()};
+                }
+            }
+            else if (argv_str == "--cst-i")
+            {
+                float img;
+                if (parse_arg(img, i, argc, argv, should_quit, retval))
+                {
+                    julia_c = {julia_c.real(), img};
+                }
+            }
+            else if (argv_str == "--inc-r")
+            {
+                float real;
+                if (parse_arg(real, i, argc, argv, should_quit, retval))
+                {
+                    julia_c_displacement = {real, julia_c_displacement.imag()};
+                }
+            }
+            else if (argv_str == "--inc-i")
+            {
+                float img;
+                if (parse_arg(img, i, argc, argv, should_quit, retval))
+                {
+                    julia_c_displacement = {julia_c_displacement.real(), img};
+                }
+            }
+            else if (argv_str == "--width")
+            {
+                parse_arg(width, i, argc, argv, should_quit, retval);
+            }
+            else if (argv_str == "--height")
+            {
+                parse_arg(height, i, argc, argv, should_quit, retval);
+            }
+        }
+
+        return should_quit;
+    }
+
+  private:
+    static constexpr unsigned int s_default_window_witdh{640};
+    static constexpr unsigned int s_default_window_height{480};
+
+    static constexpr complex s_default_julia_c{0.355534f, -0.337292f};
+
+    // Displacement value when key pressing r/R or i/I
+    static constexpr complex s_julia_c_displacement{0.001f, 0.001f};
+
+    // Number of iterations before stating the norm of the series reaches infinity
+    static constexpr unsigned int s_default_iterations{64};
+
+    // This is infinity here ;-)
+    static constexpr float s_default_infinity{1000.f};
+};
+
+template <typename T> using unique_ptr_custom_delete = std::unique_ptr<T, std::function<void(T*)>>;
 } // namespace
 
 extern "C" int
-main(int /* argc */, char** /* argv */)
+main(int argc, char** argv)
 {
+    int retval = EXIT_FAILURE;
+
+    app_config config;
+    bool should_quit = config.parse_cmdline(argc, argv, retval);
+    if (should_quit)
+    {
+        return retval;
+    }
+
     SDL_Init(SDL_INIT_EVERYTHING);
     SCOPE_EXIT([]() { SDL_Quit(); });
 
     SDL_Window* window = nullptr;
-    window =
-        SDL_CreateWindow("Julia Set viewer", s_window_default_witdh, s_window_default_height, 0);
+    window = SDL_CreateWindow("Julia Set viewer", config.width, config.height, 0);
     if (!window)
     {
         std::printf("error: failed creating window\n");
-        return EXIT_FAILURE;
+        return retval;
     }
     SCOPE_EXIT([window]() { SDL_DestroyWindow(window); });
 
@@ -57,7 +201,7 @@ main(int /* argc */, char** /* argv */)
     if (!renderer)
     {
         std::printf("error: failed creating renderer\n");
-        return EXIT_FAILURE;
+        return retval;
     }
     SCOPE_EXIT([renderer]() { SDL_DestroyRenderer(renderer); });
 
@@ -67,7 +211,7 @@ main(int /* argc */, char** /* argv */)
     if (r)
     {
         std::printf("error: failed retrieving renderer output size\n");
-        return EXIT_FAILURE;
+        return retval;
     }
 
     SDL_Texture* texture = nullptr;
@@ -76,7 +220,7 @@ main(int /* argc */, char** /* argv */)
     if (!texture)
     {
         std::printf("error: failed creating the render texture\n");
-        return EXIT_FAILURE;
+        return retval;
     }
 
     unique_ptr_custom_delete<unsigned char> buffer{
@@ -85,7 +229,7 @@ main(int /* argc */, char** /* argv */)
     if (!buffer)
     {
         std::printf("error: failed creating the render texture buffer\n");
-        return EXIT_FAILURE;
+        return retval;
     }
 
     unique_ptr_custom_delete<unsigned char> color_palette{
@@ -94,7 +238,7 @@ main(int /* argc */, char** /* argv */)
     if (!color_palette)
     {
         std::printf("error: failed allocating color palette");
-        return EXIT_FAILURE;
+        return retval;
     }
     for (size_t i = 0; i < static_cast<size_t>(256); ++i)
     {
@@ -141,19 +285,19 @@ main(int /* argc */, char** /* argv */)
                     break;
                 case SDL_SCANCODE_R:
                     if (e.key.keysym.mod & SDL_KMOD_SHIFT)
-                        julia_c += complex{julia_c_displacement.real(), 0.f};
+                        config.julia_c += complex{config.julia_c_displacement.real(), 0.f};
                     else
-                        julia_c -= complex{julia_c_displacement.real(), 0.f};
+                        config.julia_c -= complex{config.julia_c_displacement.real(), 0.f};
                     break;
                 case SDL_SCANCODE_I:
                     if (e.key.keysym.mod & SDL_KMOD_SHIFT)
-                        julia_c += complex{0.f, julia_c_displacement.imag()};
+                        config.julia_c += complex{0.f, config.julia_c_displacement.imag()};
                     else
-                        julia_c -= complex{0.f, julia_c_displacement.imag()};
+                        config.julia_c -= complex{0.f, config.julia_c_displacement.imag()};
                     break;
                 case SDL_SCANCODE_P:
-                    std::printf("c = %f + %fi, pos = %f + %fi\n", julia_c.real(), julia_c.imag(),
-                                pos.real(), pos.imag());
+                    std::printf("c = %f + %fi, pos = %f + %fi\n", config.julia_c.real(),
+                                config.julia_c.imag(), pos.real(), pos.imag());
                     break;
                 default:
                     // do nothing
@@ -169,19 +313,19 @@ main(int /* argc */, char** /* argv */)
             {
                 complex z{scale * (float(x) - float(w) / 2.f) / float(w),
                           scale * (float(y) - float(h) / 2.f) / float(h)};
-                int i;
-
                 z += pos;
-                for (i = 0; i < s_iterations; ++i)
+
+                unsigned int i;
+                for (i = 0; i < config.iterations; ++i)
                 {
-                    z = z * z + julia_c;
-                    if (std::norm(z) > s_arbitrary_big_norm)
+                    z = z * z + config.julia_c;
+                    if (std::norm(z) > config.infinity)
                     {
                         break;
                     }
                 }
 
-                float lumaf = float(i) / float(s_iterations);
+                float lumaf = float(i) / float(config.iterations);
                 size_t luma = static_cast<size_t>(std::max(std::min(lumaf * 255.f, 255.f), 0.f));
                 unsigned char const* color = &color_palette.get()[luma * 3];
 
@@ -198,5 +342,6 @@ main(int /* argc */, char** /* argv */)
         SDL_RenderPresent(renderer);
     }
 
-    return EXIT_SUCCESS;
+    retval = EXIT_SUCCESS;
+    return retval;
 }
